3699d1dfb5b132fe07cde2e0f755e23ba10d8f00
Fredrick W. Warren <fredw@northriverboats.com>
2019-03-14 12:51:26 -0700

    code now checks for zero hulls and emails operators

--- check_for_hulls.py
+++ check_for_hulls.py
@@ -11,36 +11,20 @@ load_dotenv()
 
 
 #### HEAR BE DRAGONS
-def push_sheet(xlshulls):
+def check_hulls():
     forwarder = bgtunnel.open(ssh_user=os.getenv('SSH_USER'), ssh_address=os.getenv('SSH_HOST'), host_port=3306, bind_port=3307)
-    #forwarder = bgtunnel.open(ssh_user=os.getenv('SSH_USER'), ssh_address='10.10.200.93', host_port=3306, bind_port=3306)
 
     conn= MySQLdb.connect(host='127.0.0.1', port=3307, user=os.getenv('DB_USER'), passwd=os.getenv('DB_PASS'), db=os.getenv('DB_NAME'))
 
-    conn.query("""TRUNCATE TABLE wp_nrb_hulls""")
-    r=conn.use_result()
-
     cursor = conn.cursor()
-    sql = """
-    INSERT INTO wp_nrb_hulls (
-        hull_serial_number, dealership, model,
-        last_name, first_name, phone,
-        mailing_address, mailing_city, mailing_state, mailing_zip,
-        street_address, street_city, street_state, street_zip,
-        date_purchased, p
-    ) VALUES (
-        %s, %s, %s, 
-        %s, %s, %s, 
-        %s, %s, %s, %s, 
-        %s, %s, %s, %s, 
-        %s, %s
-    )"""
-    cursor.executemany(sql,sorted(xlshulls[:1287]))
-    conn.commit()
+    cursor.execute('SELECT COUNT(id) AS COUNT FROM wp_nrb_hulls')
+    count = cursor.fetchone()
 
     cursor.close()
     conn.close()
     forwarder.close()
+
+    return count[0]
 
 
 def mail_results(subject, body):
@@ -59,20 +43,16 @@ def mail_results(subject, body):
 
 def main():
     try:
-        xlshulls, errors_dealer, errors_boat_model, errors_hull = readsheet(xlsfile)
-        push_sheet(xlshulls)
-        if (errors_dealer or errors_boat_model or errors_hull):
-            body = format_errors(errors_dealer, errors_boat_model, errors_hull)
-            mail_results('Dealer Inventory Data Entry Errors', body)
-    except OSError:
-        mail_results(
-            'Registrations and Dealer Inventory Sheet is Open',
-            'Registrations and Dealer Inventory Sheet is Open, website can not be updated'
-        )
+        result = check_hulls()
+        if (result == 0):
+            mail_results(
+                'Check for Hulls Error',
+                '<p>There is a problem with the website, there are no hulls listed for any dealer in wp_nrb_hulls</p>'
+            )
     except Exception as e:
         mail_results(
-            'Registrations and Dealer Inventory Sheet Processing Error',
-            '<p>Website can not be updated due to error on sheet:<br />\n' + e + '</p>'
+            'Check for Hulls Error',
+            '<p>There was an error while trying to count the number of hulls in wp_nrb_hulls</p>'
         )
 
 if __name__ == "__main__":
